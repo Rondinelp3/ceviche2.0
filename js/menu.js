d63c4e954c4eb3c64a588bb616be2e6ae8bcc6be
Rondinelp3 <daniel_rondinelp3@hotmail.com>
2020-02-08 01:50:22 -0500

    primer commit

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+var btnMenu1 = document.getElementById('btn-menu');
+var nav = document.getElementById('nav');
+
+btnMenu1.addEventListener('click',function(){
+nav.classList.toggle('mostrar');
+})
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel();
+});
